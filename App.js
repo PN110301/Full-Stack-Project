a6884e67a335ce91ecc3f92cc8f6cb7c5a38cc2f
PN110301 <74809903+PN110301@users.noreply.github.com>
2021-04-08 23:51:49 +0530

    second commit

---
+++ App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Food from './Food'
+
+function App() {
+  return (
+    <div className="App">
+      <Food/>
+
+    </div>
+  );
+}
+
+export default App;
